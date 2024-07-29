a875666881a13d623b39b0f89b11c8230fc9e4d3
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-07-29 18:02:37 +0300

    create Product

---
+++ src/Pages/ProductPage/ProductPage.js
@@ -0,0 +1,9 @@
+import {ProductOverview} from '../../components/ProductOverview/ProductOverview';
+
+export default function  ProdactPage(){
+    return(
+        <>
+           <ProductOverview/>
+        </>
+    )
+}
