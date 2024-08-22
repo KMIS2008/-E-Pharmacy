c73fbdcf4f5312183666421c666c8ab041922bae
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-08-22 23:34:32 +0300

    update FeaturesSection

--- src/components/FeaturesSection/FeaturesSection.js
+++ src/components/FeaturesSection/FeaturesSection.js
@@ -1,40 +1,27 @@
 import sprite from '../../images/sprite.svg';
 import {Container, ContainerItem} from './FeaturesSection.styled';
+import { nanoid } from 'nanoid';
+
+const features=[
+    "Take user orders form online",
+    "Create your shop profile",
+    "Manage your store",
+    "Get more orders",
+    "Storage shed"
+]
 
 
 export const FeaturesSection=()=>{
     return(
         <Container>
-            <ContainerItem>
-                <svg width={20} height={20}>
-                <use xlinkHref={sprite + '#icon-lightning-01'}/>
-                </svg>
-                <p>Take user orders form online</p>
-            </ContainerItem>
-            <ContainerItem>
-                <svg width={20} height={20}>
-                <use xlinkHref={sprite + '#icon-lightning-01'}/>
-                </svg>
-                <p>Create your shop profile</p>
-            </ContainerItem>
-            <ContainerItem>
-                <svg width={20} height={20}>
-                <use xlinkHref={sprite + '#icon-lightning-01'}/>
-                </svg>
-                <p>Manage your store</p>
-            </ContainerItem>
-            <ContainerItem>
-                <svg width={20} height={20}>
-                <use xlinkHref={sprite + '#icon-lightning-01'}/>
-                </svg>
-                <p>Get more orders</p>
-            </ContainerItem>
-            <ContainerItem>
-                <svg width={20} height={20}>
-                <use xlinkHref={sprite + '#icon-lightning-01'}/>
-                </svg>
-                <p>Storage shed</p>
-            </ContainerItem>
+            {features.map((feature) => (
+                <ContainerItem key={nanoid()}>
+                    <svg width={20} height={20}>
+                        <use xlinkHref={sprite + '#icon-lightning-01'} />
+                    </svg>
+                    <p>{feature}</p>
+                </ContainerItem>
+            ))}
         </Container>
     )
 }
